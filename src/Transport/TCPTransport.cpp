d12ef0a4d1c6c9df645a345525efa2902001f94d
michal.szwaj <michal.szwaj@o2.pl>
2018-03-15 22:33:57 +0100

    Fix typo

--- src/Transport/TCPTransport.cpp
+++ src/Transport/TCPTransport.cpp
@@ -34,7 +34,7 @@ TCPTransport::TCPTransport(boost::asio::io_service& ioService, tcp::ITCPEndpoint
 
 void TCPTransport::enqueueReceive(common::DataBuffer buffer)
 {
-    auto receivePromise = tcp::ITCPEndpoint::Promise::defer(receivesendStrand_);
+    auto receivePromise = tcp::ITCPEndpoint::Promise::defer(receiveStrand_);
     receivePromise->then([this, self = this->shared_from_this()](auto bytesTransferred) {
             this->receiveHandler(bytesTransferred);
         },
